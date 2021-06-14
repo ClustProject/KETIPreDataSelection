ec31f33c26c48d2dc37dad55e733ef4d5d66f09a
Moon Jaewon <jaewonmoo.keti@gmail.com>
2021-06-14 11:19:00 +0900

    Release version

--- manual_data_selection/static_test_dataSet.py
+++ manual_data_selection/static_test_dataSet.py
@@ -1,7 +1,8 @@
 
-def set_integratedDataInfo(start, end):
-    intDataInfo = {"db_info":
-                   [{"db_name":"INNER_AIR","measurement":"HS1","domain":"farm","subdomain":"airQuality","start":str(start),"end":str(end)},
-                 {"db_name":"OUTDOOR_AIR","measurement":"sangju","domain":"city","subdomain":"airQuality","start":str(start),"end":str(end)},
-                 {"db_name":"OUTDOOR_WEATHER","measurement":"sangju","domain":"city","subdomain":"weather","start":str(start),"end":str(end)}]}
-    return intDataInfo
+def set_integratedDataInfo(start, end, partial_dataset_type):
+    if partial_dataset_type =='farm1':
+        intDataInfo = {"db_info":
+                       [{"db_name":"INNER_AIR","measurement":"HS1","domain":"farm","subdomain":"airQuality","start":str(start),"end":str(end)},
+                     {"db_name":"OUTDOOR_AIR","measurement":"sangju","domain":"city","subdomain":"airQuality","start":str(start),"end":str(end)},
+                     {"db_name":"OUTDOOR_WEATHER","measurement":"sangju","domain":"city","subdomain":"weather","start":str(start),"end":str(end)}]}
+        return intDataInfo
