2880c0c635e7b22b64d5f3091fd769e5bed20772
Moon Jaewon <jaewonmoo.keti@gmail.com>
2021-06-25 15:10:25 +0900

    minor

--- manual_data_selection/static_test_dataSet.py
+++ manual_data_selection/static_test_dataSet.py
@@ -2,7 +2,7 @@
 def set_integratedDataInfo(start, end, partial_dataset_type):
     if partial_dataset_type =='farm1':
         intDataInfo = {"db_info":
-                       [{"db_name":"INNER_AIR","measurement":"HS1","domain":"farm","subdomain":"airQuality","start":str(start),"end":str(end), "frequency":'2min'},
-                     {"db_name":"OUTDOOR_AIR","measurement":"sangju","domain":"city","subdomain":"airQuality","start":str(start),"end":str(end), "frequency":'60min'},
-                     {"db_name":"OUTDOOR_WEATHER","measurement":"sangju","domain":"city","subdomain":"weather","start":str(start),"end":str(end), "frequency":'60min'}]}
+                       [{"db_name":"INNER_AIR","measurement":"HS1","start":str(start),"end":str(end)},
+                     {"db_name":"OUTDOOR_AIR","measurement":"sangju","start":str(start),"end":str(end)},
+                     {"db_name":"OUTDOOR_WEATHER","measurement":"sangju","start":str(start),"end":str(end)}]}
         return intDataInfo
