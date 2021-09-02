b957fa775ca6c7573dee17b25aea8ad24c2f2685
Moon Jaewon <jaewonmoo.keti@gmail.com>
2021-09-02 19:01:44 +0900

    modi for kweather

--- manual_data_selection/static_test_dataSet.py
+++ manual_data_selection/static_test_dataSet.py
@@ -5,4 +5,10 @@ def set_integratedDataInfo(start, end, partial_dataset_type):
                        [{"db_name":"INNER_AIR","measurement":"HS1","start":str(start),"end":str(end)},
                      {"db_name":"OUTDOOR_AIR","measurement":"sangju","start":str(start),"end":str(end)},
                      {"db_name":"OUTDOOR_WEATHER","measurement":"sangju","start":str(start),"end":str(end)}]}
+    if partial_dataset_type =='KWeather':
+        intDataInfo = {"db_info":
+                       [{"db_name":"INNER_AIR","measurement":"HS1","start":str(start),"end":str(end)},
+                     {"db_name":"OUTDOOR_AIR","measurement":"sangju","start":str(start),"end":str(end)},
+                     {"db_name":"OUTDOOR_WEATHER","measurement":"sangju","start":str(start),"end":str(end)}]}
+        
         return intDataInfo
