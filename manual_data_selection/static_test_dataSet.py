67fb905e32cfd406e263cbe40e81e99c2c4ca89f
Moon Jaewon <jaewonmoo.keti@gmail.com>
2021-09-02 14:06:32 +0900

    air-farm

--- manual_data_selection/static_test_dataSet.py
+++ manual_data_selection/static_test_dataSet.py
@@ -1,6 +1,6 @@
 
 def set_integratedDataInfo(start, end, partial_dataset_type):
-    if partial_dataset_type =='farm1':
+    if partial_dataset_type =='VIBES':
         intDataInfo = {"db_info":
                        [{"db_name":"INNER_AIR","measurement":"HS1","start":str(start),"end":str(end)},
                      {"db_name":"OUTDOOR_AIR","measurement":"sangju","start":str(start),"end":str(end)},
