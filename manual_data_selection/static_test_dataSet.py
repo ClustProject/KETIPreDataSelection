6157759b5e3d1823dd53f69033f88631f723c91f
Moon Jaewon <jaewonmoo.keti@gmail.com>
2021-09-07 18:33:33 +0900

    error modification

--- manual_data_selection/static_test_dataSet.py
+++ manual_data_selection/static_test_dataSet.py
@@ -5,10 +5,9 @@ def set_integratedDataInfo(start, end, partial_dataset_type):
                        [{"db_name":"INNER_AIR","measurement":"HS1","start":str(start),"end":str(end)},
                      {"db_name":"OUTDOOR_AIR","measurement":"sangju","start":str(start),"end":str(end)},
                      {"db_name":"OUTDOOR_WEATHER","measurement":"sangju","start":str(start),"end":str(end)}]}
-    if partial_dataset_type =='KWeather':
+    elif partial_dataset_type =='KWeather':
         intDataInfo = {"db_info":
-                       [{"db_name":"INNER_AIR","measurement":"HS1","start":str(start),"end":str(end)},
-                     {"db_name":"OUTDOOR_AIR","measurement":"sangju","start":str(start),"end":str(end)},
-                     {"db_name":"OUTDOOR_WEATHER","measurement":"sangju","start":str(start),"end":str(end)}]}
+                       [{"db_name":"air_indoor_유치원","measurement":"ICW0W2000132","start":str(start),"end":str(end)},
+                     {"db_name":"OUTDOOR_AIR","measurement":"seoul","start":str(start),"end":str(end)}]}
         
-        return intDataInfo
+    return intDataInfo
