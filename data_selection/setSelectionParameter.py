c8a5ae8b3cf5c26eacacdbe508f902e5d20a09a7
jwmoonP <jaewonmoon.keti@gmail.com>
2022-06-07 18:00:16 +0900

    update

--- data_selection/setSelectionParameter.py
+++ data_selection/setSelectionParameter.py
@@ -21,3 +21,21 @@ def set_integratedDataInfoByDBSet(start, end, db_set):
     return intDataInfo
 
 
+def makeIntDataInfoSet(dataInfo, start, end):
+    intDataInfo ={}
+    db_info=[]
+    
+    for db_index, db_one in enumerate(dataInfo):
+        db_set={}
+        db_set['start'] = start
+        db_set['end'] = end
+        db_set['db_name'] = db_one[0]
+        db_set['measurement'] = db_one[1]
+        if len(db_one)>2:
+            db_set['tag_info'] = db_one[2]
+        db_info.append(db_set)
+
+    intDataInfo["db_info"] = db_info
+    return intDataInfo
+
+
