b49c740be84531137292710c489ce655281824be
jwmoonP <jaewonmoon.keti@gmail.com>
2022-08-11 21:19:47 +0900

    update tagkey issue

--- data_selection/setSelectionParameter.py
+++ data_selection/setSelectionParameter.py
@@ -32,7 +32,8 @@ def makeIntDataInfoSet(dataInfo, start, end):
         db_set['db_name'] = db_one[0]
         db_set['measurement'] = db_one[1]
         if len(db_one)>2:
-            db_set['tag_info'] = db_one[2]
+            db_set ['tag_key']= db_one[2]
+            db_set ['tag_value']= db_one[3]
         db_info.append(db_set)
 
     intDataInfo["db_info"] = db_info
